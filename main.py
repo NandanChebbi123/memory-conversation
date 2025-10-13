ad40d5cd0bff812f9617c9cf6890cc5050b919a1
Akhila <akhiping2@gmail.com>
2025-10-13 17:34:07 +0530

    Initial commit: add memory_conversation project

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
 
-
 for var in ["HTTP_PROXY", "HTTPS_PROXY", "http_proxy", "https_proxy"]:
     if var in os.environ:
         del os.environ[var]
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 import langchain
 print(langchain.__version__)
+
+import streamlit as st
+import openai
+print(openai.__version__)
